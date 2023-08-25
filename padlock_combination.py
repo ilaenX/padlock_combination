f3c25cf3ef969eea722b10519643bd796859ffa0
ilaenX <64144495+ilaenX@users.noreply.github.com>
2023-08-25 19:46:57 +0000

    fix and optimization
    The code changes made to address your specific requirements were primarily focused on correctly generating all possible permutations of the remembered digits combined with the remaining numbers to create valid passwords. Here's a summary of the changes:
    
    1. Modified the nested loops structure to handle various cases:
       - When the user doesn't remember anything, combinations are used to generate passwords.
       - When the user remembers some digits but not their placement, permutations are used to generate passwords.
    
    2. The `itertools.permutations` function is used to create all possible permutations of the remembered digits and the missing digits in the password.
    
    These changes ensure that all possible valid passwords are generated based on the user's remembered digits and the desired password length.
    
    As for the optimization notation, you seem to be referring to the concept of "Big O" notation. In computer science, Big O notation is used to describe the upper bound of the time complexity of an algorithm in terms of the input size. It provides an asymptotic approximation of how the algorithm's performance scales as the input grows larger.
    
    The optimized version of the code has a time complexity of approximately O(N^2), where N is the difference between `end_number` and `start_number`. This is an improvement over the original version, which had a time complexity of O(N! * N). The new version's time complexity is better because it avoids unnecessary permutations and uses sets for faster membership testing, resulting in more efficient execution for larger input ranges.

--- padlock_combination.py
+++ padlock_combination.py
@@ -3,13 +3,8 @@ Name: Daniel Adeyelu
 
 File: padlock_combination.py
 
-Description: The "Password Combination Generator for Padlock" 
-is a Python tool that assists users in creating potential padlock 
-passwords. Users input remembered digits, and the program generates 
-all possible combinations for the remaining digits, using a systematic 
-approach of combinations and permutations. This project aids users in 
-recovering forgotten padlock codes by providing an array of possible 
-passwords for quick identification.
+Description: The "Password Combination Generator for Padlock" is a Python tool that utilises the theory of permutation and combination which assists users in creating potential padlock passwords. Users input remembered digits, and the program generates all possible combinations for the remaining digits, using a systematic approach of combinations and permutations. This project aids users in 
+recovering forgotten padlock codes by providing an array of possible passwords for quick identification.
 '''
 
 import itertools
@@ -20,8 +15,6 @@ end_number = 50
 password_length = 3
 
 def generate_padlock_passwords(start, end, length, remembered_part):
-    # Generate a list of numbers within the specified range
-    numbers = list(range(start, end + 1))
     passwords = []
 
     # Calculate the number of missing digits in the password
@@ -29,34 +22,33 @@ def generate_padlock_passwords(start, end, length, remembered_part):
 
     # Check if the remembered part is longer than the desired password length
     if missing_piece < 0:
-        # print("Remembered part is longer than the password length.")
         return passwords
 
-    # Generate combinations for the missing digits
-    for combination in itertools.combinations(numbers, missing_piece):
-        remaining_numbers = [number for number in numbers if number not in remembered_part + list(combination)]
-        # Generate permutations for the remaining numbers
-        for permutation in itertools.permutations(remaining_numbers, len(remaining_numbers)):
-            # Concatenate the remembered part, missing digits, and remaining numbers
-            combination = list(remembered_part) + list(combination) + list(permutation)
-            passwords.append(combination)
-    return passwords
+    remaining_numbers = set(range(start, end + 1)) - set(remembered_part)
 
+    if not remembered_part:  # User doesn't remember anything
+        for combination in itertools.combinations(remaining_numbers, length):
+            passwords.append(list(combination))
+    else:
+        for combination in itertools.combinations(remaining_numbers, missing_piece):
+            for permutation in itertools.permutations(remembered_part + list(combination)):
+                passwords.append(list(permutation))
+
+    return passwords
 
 print("\n-----------------------------------------")
 print("Enter the remembered part of the password")
 print("-----------------------------------------\n")
 print("Note: use spaces to separate numbers (max three numbers)\n")
 
-remembered_part = input("Numbers: ").split()
-remembered_part = list(map(int, remembered_part))
+remembered_part = list(map(int, input("Numbers: ").split()))
 
 passwords = generate_padlock_passwords(start_number, end_number, password_length, remembered_part)
 
 # Generate the total number of possible passwords
 total_passwords = len(passwords)
 
-print("Total number of possible passwords: {}".format(total_passwords))
+print("Total number of possible passwords:", total_passwords)
 print("Possible Passwords:")
-for password in passwords[:10]:
+for password in passwords[:200]:
     print(' '.join(map(str, password)))
